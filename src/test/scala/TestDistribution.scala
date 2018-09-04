e5272e6db781528eb67987d33b2b979f85e72bb4
Arthur Lui <luiarthur@gmail.com>
2018-09-04 08:53:28 -0700

    added some tests

--- src/test/scala/TestDistribution.scala
+++ src/test/scala/TestDistribution.scala
@@ -3,32 +3,21 @@ import distribution.helper.timer
 import org.apache.commons.math3.random.RandomDataGenerator
 
 
-class TestDistribution extends FunSuite {
-  def assertApprox(x:Double, y:Double, eps:Double=.1E-4, debug:Boolean=false) = {
-    val valid = math.abs(x - y) < eps
-    if (debug) {
-      if (!valid) {
-        println(s"invalid -- x: $x; y:$y")
-      } else println(s"valid -- x: $x; y:$y")
-    }
-    assert(valid)
-  }
-
-  def testWithMsg[R](msg:String)(block: => R) = {
-    test(msg) {
-      print(msg + " -- ")
-      timer {
-        block
-      }
-    }
-  }
+class TestDistribution extends TestUtil {
 
   test("Normal") {
     import distribution.continuous.Normal
     val n = new Normal(3.0, 2.0)
-    // TODO: timer
+    // Assertions
     assertApprox(n.pdf(0.5), 0.09132454)
     assertApprox(n.lcdf(0.5), -2.247626, debug=true)
+
+    // Speed test. These take much less than a second. So they're fine.
+    //val idx = (0 until 1E6.toInt)
+    //timer { print("normal pdf:  "); idx.foreach{ i =>(new Normal(3,2)).pdf(0.5) }}
+    //timer { print("normal lpdf: "); idx.foreach{ i =>(new Normal(3,2)).lpdf(0.5) }}
+    //timer { print("normal cdf:  "); idx.foreach{ i =>(new Normal(3,2)).cdf(0.5) }}
+    //timer { print("normal lcdf: "); idx.foreach{ i =>(new Normal(3,2)).lcdf(0.5) }}
   }
 }
 
--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -2,18 +2,7 @@ import org.scalatest.FunSuite
 import distribution.helper.timer
 import org.apache.commons.math3.random.RandomDataGenerator
 
-class MyFunSuite extends FunSuite {
-  def testWithMsg[R](msg:String)(block: => R) = {
-    test(msg) {
-      print(msg + " -- ")
-      timer {
-        block
-      }
-    }
-  }
-}
-
-class TestSuite extends MyFunSuite {
+class TestSuite extends TestUtil {
   // For reproducibility, don't use ThreadLocalRandom for testing.
   //import distribution.RandomPar._
 
@@ -23,48 +12,6 @@ class TestSuite extends MyFunSuite {
 
   //val commonsMathR = new RandomDataGenerator()
 
-  def round(x:Double, d:Int) = {
-    require(d >= 0)
-    val dMax = x.toString.split('.').last.size
-    val factor = math.pow(10, if(d>dMax) dMax else d)
-    (x * factor).toInt / factor
-  }
-
-
-  def mean(x:Vector[Double]) = x.sum / x.size
-  def sd(x: Vector[Double]) = {
-    val m = mean(x)
-    val ss = x.map(xi => math.pow(xi - m,2)).sum
-    math.sqrt(ss / x.size)
-  }
-  def variance(x:Vector[Double]) = math.pow(sd(x), 2)
-
-  def assertApprox(x:Double, y:Double, eps:Double=.01, debug:Boolean=false) = {
-    val valid = math.abs(x - y) < eps
-    if (debug) {
-      if (!valid) {
-        println(s"invalid -- x: $x; y:$y")
-      } else println(s"valid -- x: $x; y:$y")
-    }
-    assert(valid)
-  }
-
-  def assertApproxArray(a:Array[Double], b:Array[Double], eps:Double=1E-3, debug:Boolean=false): Unit = {
-    a.zip(b).foreach{ case(ai, bi) =>
-      assertApprox(ai, bi, eps)
-    }
-  }
-
-  def assertApproxMat(a:Array[Array[Double]], b:Array[Array[Double]], eps:Double=1E-3, debug:Boolean=false): Unit = {
-    import distribution.SpecialFunctions.printMat
-
-    val matA = a.flatten
-    val matB = b.flatten
-    matA.zip(matB).foreach{ case (ma,mb) => 
-      assertApprox(ma, mb, eps)
-    }
-  }
-  
   val n = 1E5.toInt
   println
 
